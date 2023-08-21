3e7dc0e016f73ef9eb3a417b350a6ca591d8482a
Bia-Novais <117457691+Bia-Novais@users.noreply.github.com>
2023-08-21 10:00:41 -0300

    Add files via upload

---
+++ php/enviar-prematricula.php
@@ -0,0 +1,38 @@
+<?php
+  //Variáveis
+  $nome = $_POST['nomea'];
+  $dtnasc = $_POST['datan'];
+  $nomer = $_POST['nomer'];
+  $nomedois = $_POST['nomedois'];
+  $teleone = $_POST['tele'];
+  $email = $_POST['email'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>Data de Nascimento: </b>$dtnasc</p>
+      <p><b>Nome do Responsável que nos contatou: </b>$nomer</p>
+      <p><b>Nome de um Segundo Responsável: </b>$nomedois</p>
+      <p><b>Telefone para contato: </b>$telefone</p>
+      <p><b>E-mail: </b>$email</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "bianovais08@gmail.com";
+  $assunto = "Contato pelo Site";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+?>
