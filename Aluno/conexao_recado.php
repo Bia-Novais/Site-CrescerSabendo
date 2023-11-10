d6491f0a961826ba7dbb1e9c0cc8e4aaa4321c20
Isaanine <111399296+Isaanine@users.noreply.github.com>
2023-11-10 08:21:34 -0300

    Delete Aluno directory

--- Aluno/conexao_recado.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "";
-	$dbname = "funildevendas";
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-?>
--- Aluno/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); 
-
-// Destruir a sessão atual (deslogar)
-session_destroy();
-
-// Redirecionar para a página de login ou qualquer outra página desejada
-header('Location: ../Login.php'); 
-?>
